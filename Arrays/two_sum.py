08b8aa56a36176b70983b86d9dadb0efd02b9bc8
nehal0905 <anehaldeepika@gmail.com>
2025-07-07 23:18:39 +0530

    Added Two Sum in Arrays

---
+++ Arrays/two_sum.py
@@ -0,0 +1,9 @@
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        if target - num in hashmap:
+            return [hashmap[target - num], i]
+        hashmap[num] = i
+
+# Sample Test
+print(twoSum([2, 7, 11, 15], 9))
